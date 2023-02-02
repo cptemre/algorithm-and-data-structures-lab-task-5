a5ebed2464b9a96ac9c7d16db3a1cf5d82aa4d84
cptemre <kunduraci2@gmail.com>
2023-02-02 12:14:49 +0100

    Project Upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# ALGORITHM AND DATA STRUCTURES LABORATORY
+
+## TASK 5
+
+### GENERAL INFORMATION
+
+1. This project has been prepared for a course in informatics in the first semester.
+
+2. Project is prepared in Visual Studio 2022 application in Windows 11 OS. It may not work as intended in other platforms.
+
+3. Project only contains the code in github not the project folder prepared by Visual Studio.
+
+### DESCRIPTION OF THE TASK
+
+Show in one diagram, using a loop, the algorithm for calculating the sum, sum
+squares and the arithmetic mean of consecutive natural numbers from a to b. Derive the results
+calculations in a way that allows the user to choose which of the results he wants to see.
+1. sum
+2. sum of squares
+3. arithmetic mean
+4. end of program
+The user enters a and b from the keyboard. Make sure that the initial conditions are met
+and final. Implement the algorithm in mb and C/C++
---
+++ task-5.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int a, b;
+    do
+    {
+        cout << "Please enter 2 natural numbers and first number should be smaller or equal.\n";
+        cout << "First number is: ";
+        cin >> a;
+        cout << "Second number is: ";
+        cin >> b;
+    } while (a > b);
+
+
+    bool isTrue = true;
+    while (isTrue)
+    {
+        int a1 = a;
+        int b1 = b;
+        float result = 0;
+        int numbers = 0;
+        int option;
+        cout << "1- sum\n";
+        cout << "2- sum of squares\n";
+        cout << "3- arithmetic mean\n";
+        cout << "4- end of program\n";
+        cin >> option;
+        switch (option)
+        {
+        case 1:
+            while (a1 <= b1)
+            {
+                result += a1;
+                a1++;
+            }
+            cout << "Sum is: " << result << endl;
+            break;
+        case 2:
+            while (a1 <= b1)
+            {
+                result += a1 * a1;
+                a1++;
+            }
+            cout << "Sum of squares is: " << result << endl;
+            break;
+        case 3:
+            while (a1 <= b1)
+            {
+                result += a1;
+                a1++;
+                numbers++;
+            }
+            result /= numbers;
+            cout << "Arithmetic mean is is: " << result << endl;
+            break;
+        case 4:
+            isTrue = false;
+            break;
+        default:
+            cout << "Please select and option between 1-4" << endl;
+            break;
+        }
+    }
+    return 0;
+}
